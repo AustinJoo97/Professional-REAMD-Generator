ac0ca025345c6337dd278600e1b341b2aac0d1e6
AustinJoo97 <austinjoo97@gmail.com>
2021-04-12 15:02:48 -0400

    create array of basic questions essential for generating a quality readme

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = ['What is the name of the application', ];
+const questions = ['What is the name of the application?', 'In 2-3 sentences, please describe this project, its purpose, and why it should be used!', 'What technologies were used in the making of this application?', 'How do you intall, setup and run the application?', 'Do you have a deployed project link?', 'Who are the main contributors for this project?', 'Are there any licenses affiliated with this application?'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}

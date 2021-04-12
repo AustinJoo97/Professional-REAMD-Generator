df30ce214b76b422db7fe67c975a642ce356f02f
AustinJoo97 <austinjoo97@gmail.com>
2021-04-12 15:48:42 -0400

    have all essential questions and then some being asked to the user upon running the commands in bash

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = ['What is the name of the application?', 'Do you have a deployed project link?', 'Please describe this project,its purpose, intallation, and setup!', 'What technologies were used in the making of this application? Please separate each using commas!', 'Who are the main contributors for this project?', 'What tests were performed to determine the proper functionality of this application?','What license is affiliated with this application?', 'What is an email '];
+const questions = ['What is the name of the application?', 'Do you have a deployed project link?', 'Please describe this project,its purpose, intallation, and setup!', 'What technologies were used in the making of this application? Please separate each using commas!', 'Who are the main contributors for this project?', 'What tests were performed to determine the proper functionality of this application?','What license is affiliated with this application?', 'What is your GitHub username for additional questions or concerns?', 'What is your email address for additional questions or concerns?'];
 const licenses = {
     'Academic Free License v3.0': 'afl-3.0',
     'Apache license 2.0': 'apache-2.0',
@@ -82,7 +82,17 @@ function init() {
             message: questions[6],
             name: 'licenses',
             choices: Object.keys(licenses)
-        }
+        },
+        {
+            type: 'input',
+            message: questions[7],
+            name: 'gitHubUsername'
+        },
+        {
+            type: 'input',
+            message: questions[8],
+            name: 'email'
+        },
     ])
     .then((response) => {
         console.log(response);
